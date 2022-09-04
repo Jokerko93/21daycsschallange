5e5711a091a62260bdf6d259c6e6966b94b26ece
Roman Daru <roman.daru.ml@gmail.com>
2022-09-04 19:50:54 +0200

    Updated navbar's smooth transition on mobile

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ menuBtn.addEventListener("click", () => {
     menuBtn.classList.add("open");
     menuOpen = true;
     navList.classList.add("expanded");
+    // navList.style.transition = "all 5s";
     navListCollapsed = false;
   } else {
     menuBtn.classList.remove("open");
